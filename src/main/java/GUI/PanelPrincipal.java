5c40afb6c9081e453add33c5d6ba4cbd229de1a3
Alonso <alvergara2025@udec.cl>
2026-06-06 13:42:11 -0400

    feat: Integración PanelComprador a PanelPrincipal

--- src/main/java/GUI/PanelPrincipal.java
+++ src/main/java/GUI/PanelPrincipal.java
@@ -1,25 +1,24 @@
 package GUI;
-
 import java.awt.Color;
 import java.awt.Graphics;
 import javax.swing.JPanel;
-
 import Logica.*;
 
 
 public class PanelPrincipal extends JPanel {
     private PanelExpendedor exp;
+    private PanelComprador comp;
     public PanelPrincipal(){
-
-        exp = new PanelExpendedor (new Expendedor(6), 50, 50);
+        exp=new PanelExpendedor(new Expendedor(6), 50, 50);
+        comp=new PanelComprador(new Comprador(),450,50);
         this.setBackground(Color.white);
     }
 
     @Override
     public void paintComponent(Graphics g){
         super.paintComponent(g);
-
         exp.paintComponent(g);
+        comp.paintComponent(g);
 
     }
 }
